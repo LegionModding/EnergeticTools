1df7e9dc69f4c583905155ff58d00d08cf79da02
LegionModding <legion@legionmodding.com>
2022-01-28 04:11:31 +0000

    Fix compile error

--- src/main/java/com/legionmodding/energetictools/util/Reference.java
+++ src/main/java/com/legionmodding/energetictools/util/Reference.java
@@ -1,29 +1,6 @@
 package com.legionmodding.energetictools.util;
 
-import com.legionmodding.energetictools.handler.registry.BlockRegistry;
-import com.legionmodding.energetictools.handler.registry.ItemRegistry;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-
 public class Reference
 {
     public static final String MOD_ID = "energetictools";
-
-    public static final ItemGroup CREATIVE_TAB_BLOCKS = new ItemGroup("creativeTabBlocks")
-    {
-        @Override
-        public ItemStack makeIcon()
-        {
-            return new ItemStack(BlockRegistry.CHARGER.get());
-        }
-    };
-
-    public static final ItemGroup CREATIVE_TAB_ITEMS = new ItemGroup("creativeTabItems")
-    {
-        @Override
-        public ItemStack makeIcon()
-        {
-            return new ItemStack(ItemRegistry.BASIC_ENERGETIC_AXE.get());
-        }
-    };
 }
